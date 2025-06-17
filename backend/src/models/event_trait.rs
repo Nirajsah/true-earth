5b67c8bc9e0e9208b30487aaa45e915f892d2e37
Nirajsah <nirajsahy4aj@gmail.com>
2025-06-17 12:08:28 +0530

    Refactor: Remove geographical, pollution, weather, and climate models and handlers
    - Deleted geographical.rs, pollution.rs, weather.rs handlers and their corresponding functions.
    - Removed climate.rs, event_trait.rs, geographical.rs, pollution.rs, and weather_events.rs models.
    - Eliminated services related to climate, geographical, pollution, and weather events.
    - Introduced new llm_service.proto file for embedding and prompt services.

--- backend/src/models/event_trait.rs
+++
@@ -1,8 +0,0 @@
-use crate::services::ai::AiServiceClient;
-use anyhow::Error;
-use async_trait::async_trait;
-
-#[async_trait]
-pub trait Event {
-    async fn get_embeddings(&self, ai_service: AiServiceClient) -> Result<Vec<f64>, Error>;
-}
